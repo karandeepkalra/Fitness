fa1ce4a92d1553c3ca1b01494cd8bfc7023003b6
karandeepkalra <122427044+karandeepkalra@users.noreply.github.com>
2025-01-01 21:00:55 +0530

    My project

--- Exercise/src/Components/Files/Banner/Banner.jsx
+++ Exercise/src/Components/Files/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import banner from '../../../assets/banner.jpg'
-import './Banner.css'
-import { Link } from 'react-router-dom';
+
+import './banner.css'
+
 import DoctorLandingPage from '../LandingPage/DoctorLandingPage';
 
 
